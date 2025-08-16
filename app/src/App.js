f73c5a231ac35819741f0b751fb4434d3642cf2a
Antonio Fernando <jufer376@gmail.com>
2025-08-16 18:21:07 -0300

    Linguagem
    "EN" para "PT-BR"

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { db } from "./firebaseconection"; 
-import {collection, addDoc, setDoc, doc} from 'firebase/firestore'
+import {doc,collection, addDoc, setDoc, getDoc, getDocs} from 'firebase/firestore'
 
 import './app.css'
 
@@ -10,6 +10,7 @@ function App() {
   
   const [titulo, setTitulo] = useState('');
   const [autor, setAutor] = useState('');
+  const [post, setPosts] = useState([]);
 
 
 
@@ -43,8 +44,43 @@ function App() {
       .catch(( error)=> {
       console.log("GEROU ERRO" + error)
   })
-  }
+  };
 
+  // ESSA É UMA FORMA DE BUSCAR O ITEM NO BANCO DE DADOS
+  /* async function buscarPost(){
+
+    const posRef = doc(db, "posts", "123")
+
+    await getDoc(posRef)
+    .then((snapshot)=>{
+      setAutor(snapshot.data().autor);
+      setTitulo(snapshot.data().titulo);
+    }).catch((error) => {
+       console.log("GEROU ERRO" + error)
+    })
+  } */
+
+
+    async function buscarPost(){;
+      const posRef = collection(db, "posts")
+      await getDocs(posRef)
+      .then((snapshot)=>{
+
+          let lista = [];
+          
+          snapshot.forEach((doc)=>{
+            lista.push({
+              id: doc.id,
+              titulo: doc.data().titulo,
+              autor: doc.data().autor,
+
+            })
+          })
+
+          setPosts(lista);
+      }).catch((error)=>
+      alert("ERRO"))
+    }
  return (
    <div>
       <h1>ReactJS + Firebase :) </h1>
@@ -74,6 +110,21 @@ function App() {
         
 
         <button onClick={handleAdd} >Cadastrar</button>
+         <button onClick={buscarPost}>
+          Buscar post
+         </button>
+         <ul>
+          {post.map((post)=> {
+            return(
+              <li key={post.id}>
+                <span>Titulo: {post.titulo}</span> <br/>
+                <span>Autor: {post.autor}</span>  <br/> <br/>
+              </li>
+            )
+          }
+          
+          )}
+         </ul>
       </div>
    </div>
   );
